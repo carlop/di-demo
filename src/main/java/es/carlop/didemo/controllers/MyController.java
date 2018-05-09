07777978bc4d67690c158b0719fdd84f5d79115a
Carlos López <yo@carlop.es>
2018-05-09 23:20:09 +0200

    Primary Beans

--- src/main/java/es/carlop/didemo/controllers/MyController.java
+++ src/main/java/es/carlop/didemo/controllers/MyController.java
@@ -1,13 +1,20 @@
 package es.carlop.didemo.controllers;
 
+import es.carlop.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
         System.out.println("Hello!");
 
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }
--- src/main/java/es/carlop/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/es/carlop/didemo/controllers/PropertyInjectedController.java
@@ -2,12 +2,14 @@ package es.carlop.didemo.controllers;
 
 import es.carlop.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingService greetingServiceImpl;
 
     public String sayHello() {
